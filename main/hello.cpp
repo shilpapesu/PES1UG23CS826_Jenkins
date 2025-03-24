1ee3ec53f9294fdd6a763fd509d624b878f15460
SHILPA SHREE R <168258252+shilpapesu@users.noreply.github.com>
2025-03-24 11:20:52 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "PES1UG23CS826"
+  cout << "PES1UG23CS826";
   return 0;
 }
